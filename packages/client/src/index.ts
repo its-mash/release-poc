549b73dc2d718727233c702e441015c4e2ddedbc
Mohammod Al Amin Ashik <maa.ashik00@gmail.com>
2025-04-18 19:33:27 +0800

    Release poc to develop (#2) (#3)
    * adds changesets
    
    * adds branch restrictions
    
    * adds pnpm i
    
    * refactors package depdency
    
    * add tests
    
    * adds changesets
    
    * adds preview publishing
    
    * adds chmod
    
    * moves preview to after unit test
    
    * adds fetch history
    
    * adds preview failing
    
    * adds turbo
    
    * Remove .turbo directory from repository and add to .gitignore
    
    ---------
    
    Co-authored-by: Mohammod Al Amin Ashik <alamin.ashik@sitecore.com>

---
+++ packages/client/src/index.ts
@@ -0,0 +1,6 @@
+import { coreDummy } from "@its-mash/core";
+
+// Dummy code for client
+export function clientDummy(): string {
+  return "client dummy + " + coreDummy();
+}
---
+++ packages/xmc/src/index.ts
@@ -0,0 +1,6 @@
+import { clientDummy } from "@its-mash/client";
+
+// Dummy code for xmc
+export function xmcDummy(): string {
+  return "xmc dummy + " + clientDummy();
+}
