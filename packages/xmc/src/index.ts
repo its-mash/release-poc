29f24cc5eb3e885f62cdc704479d592b99b4520a
Mohammod Al Amin Ashik <maa.ashik00@gmail.com>
2025-04-22 19:02:47 +0800

    Feature/one (#6)
    * xmc ++
    
    * adds changesets
    
    ---------
    
    Co-authored-by: Mohammod Al Amin Ashik <alamin.ashik@sitecore.com>

--- packages/xmc/src/index.ts
+++ packages/xmc/src/index.ts
@@ -2,5 +2,5 @@ import { clientDummy } from "@its-mash/client";
 
 // Dummy code for xmc
 export function xmcDummy(): string {
-  return "xmc dummy +" + clientDummy();
+  return "xmc dummy ++" + clientDummy();
 }
